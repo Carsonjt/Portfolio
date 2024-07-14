6ca89b9724203c6ee277618260c254e8c44a7d99
Carson Todd <ctodd@certispec.com>
2024-07-13 18:52:05 -0700

    Design pivot

--- src/app/page.js
+++ src/app/page.js
@@ -9,31 +9,13 @@ import WorkExperience from "@/components/home/workexperience/WorkExperience";
 import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
-  console.log(1 + "1" - 1);
   return (
-    <div className="bg-orange-50">
-      {/* HEADING BAR */}
-      <div className="bg-white">
-        <Header />
-      </div>
+    <div className="">
+      <Header />
 
-      {/* MAIN PAGE */}
-      <div className="pt-10 mx-auto lg:max-w-screen-xl flex flex-row gap-x-4">
-        {/* Left Column*/}
-        <div className="basis-3/4">
-          <AboutMe />
-        </div>
-
-        {/* Right Column*/}
-        <div className="basis-1/4">
-          <Education />
-        </div>
-      </div>
-      {/* TECHNOLOGIES */}
-      <div className="pt-10 mx-auto lg:max-w-screen-xl">
-        <Technologies />
-        <WorkExperience />
-      </div>
+      <AboutMe />
+      <Technologies />
+      <WorkExperience />
     </div>
   );
 }
--- src/components/home/AboutMe.js
+++ src/components/home/AboutMe.js
@@ -1,23 +1,36 @@
+import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+
+import SoftwareVector from "../../static/img/SoftwareVector.jpg";
 
 export default function AboutMe() {
   return (
     <>
-      <div className="mt-3 m-0 p-3 bg-gray-700 flex rounded-t-lg"></div>
-      <div className="min-h-40 m-0 p-3 bg-gray-500 text-white font-mono tracking-tighter rounded-b-lg">
-        <span className="text-green-500">{"$: /home > "}</span>
-        <span> aboutme</span>
-        <div>
-          <TypeAnimation
-            sequence={[
-              // Same substring at the start will only be typed out once, initially
-              " Hello, I'm a passionate software developer interested in pursuing opportunities in the US. I have been coding for over 6 years, with 2+ years of professional experience within the financial and oil and gas industries. I have experience working in both front-end and back-end technologies, though I have a preference for backend work. Explore my projects, experience and work history to learn more about me!",
-              1000,
-            ]}
-            wrapper="span"
-            speed={99}
-            repeat={Infinity}
-          />
+      <div className="bg-sky-100 px-20 py-5 flex">
+        <div className="w-3/5 inline-block my-auto mr-5">
+          <p className="mb-3 text-6xl font-bold">Hi, I'm Carson,</p>
+          <p className="mb-5 text-5xl font-semibold">
+            a Fullstack Developer 👋
+          </p>
+          <p className="text-lg font-mono">
+            I'm a passionate software developer interested in pursuing
+            opportunities in the US. I have been coding for over 6 years, with
+            2+ years of professional experience within the financial and oil and
+            gas industries. I have experience working in both front-end and
+            back-end technologies, though I have a preference for backend work.
+            Explore my projects, experience and work history to learn more about
+            me!
+          </p>
+        </div>
+
+        <div className="w-2/5 inline-block">
+          <Image
+            alt="Software Vector"
+            src={SoftwareVector}
+            height={475}
+            width={475}
+            className="p-5"
+          ></Image>
         </div>
       </div>
     </>
